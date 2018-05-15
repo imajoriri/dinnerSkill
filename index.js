c0e8e52e012a601eb7ee8b7707e6e3ed45c8cf6a
imajoriri <30540418+imajoriri@users.noreply.github.com>
2018-05-15 23:45:58 +0900

    add setTrueIntent

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const LaunchRequestHandler = {
   }
 };
 
-// 自分で定義したインテント
+// 指定した人が晩飯いるかどうか聞く
 const getPerDataIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -34,18 +34,47 @@ const getPerDataIntentHandler = {
     }
     var params = getPerDataParams(familyNameId);
 
-    return dynamo.get(params).promise().then( data => {
-      console.log(data);
-      if(data.Item && data.Item.isNeed){
-        var msg = `${familyNameValue}さんは今晩、ご飯いるそうです。`;
-      }else{
-        var msg = `${familyNameValue}さんは今晩、ご飯いらないそうです。`;
-      }
+    if(familyNameId && familyNameValue){
+      return dynamo.get(params).promise().then( data => {
+        console.log(data);
+        if(data.Item && data.Item.isNeed){
+          var msg = `${familyNameValue}さんは今晩、ご飯いるそうです。`;
+        }else{
+          var msg = `${familyNameValue}さんは今晩、ご飯いらないそうです。`;
+        }
+        return handlerInput.responseBuilder.speak(msg).getResponse();
+      });
+    }else{
+      var msg = "指定した人は見つかりませんでした";
+      return handlerInput.responseBuilder.speak(msg).getResponse();
+    }
+
+
+  }
+};
+
+// 指定した人が晩御飯いるをセットする
+const setTruePerDataIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'setTruePerDataIntent';
+  },
+  handle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    if(request.intent.slots.family.resolutions.resolutionsPerAuthority[0].values){
+      var familyNameValue = request.intent.slots.family.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+      var familyNameId = request.intent.slots.family.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+    }
+
+    var params = setTrueOrFalseDataParams(true, familyNameId);
+    return dynamo.update(params).promise().then( (data) => {
+      var msg = `${familyNameValue}さんは今晩晩飯いるで登録しました。`;
       return handlerInput.responseBuilder.speak(msg).getResponse();
     });
 
   }
 };
+
 
 function getPerDataParams(familyNameId){
   var params = {
@@ -57,6 +86,22 @@ function getPerDataParams(familyNameId){
   return params;
 }
 
+function setTrueOrFalseDataParams(isNeed, familyNameId){
+  var params = {
+    TableName: TableName,
+    Key:{
+      user: familyNameId
+    },
+    UpdateExpression: "set isNeed = :bool",
+    ExpressionAttributeValues:{
+      ":bool": isNeed
+    },
+    ReturnValues:"UPDATED_NEW"
+  };
+  return params;
+}
+
+
 
 //exports.handler = Alexa.SkillBuilders.custom()
 exports.handler = Alexa.SkillBuilders.standard()
@@ -64,6 +109,7 @@ exports.handler = Alexa.SkillBuilders.standard()
     LaunchRequestHandler,
     other.HelpIntentHandler,
     other.SessionEndedRequestHandler,
-    getPerDataIntentHandler)
+    getPerDataIntentHandler,
+    setTruePerDataIntentHandler)
   //.addErrorHandlers(other.ErrorHandler)
   .lambda();
